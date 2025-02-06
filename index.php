e9f4cb9efbd1677d392090b33d477de4e205810e
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-02-06 13:22:43 +0100

    PDO introdution

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to our MySQL database with PDO - PHP data objects
+$dsn = 'mysql:host=localhost:3306;dbname=db;charset=utf8mb4';
+
+$pdo = new PDO($dsn, 'user','123');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo '<li>' .  $post['title'] . '</li>';
+}
