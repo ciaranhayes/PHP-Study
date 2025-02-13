e4c0d158b0f1b09fd3728863ef597140e92dd6e2
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-02-13 15:02:47 +0100

    notes page added

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'router.php';
 require 'Database.php';
 
-$db = new Database();
-
-$id = $_GET['id'];
-
-$query = "SELECT * FROM posts WHERE id = :id";
-
-$posts = $db->query($query, ["id" => $id]);
-
-dd($posts);
 
 
 
--- working.md
+++
@@ -1,1 +0,0 @@
-# Is My Committing Working 
